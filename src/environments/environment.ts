b6d601eea6e94d3405ee71d06f05451365a2ffa4
aucxence <51159780+aucxence@users.noreply.github.com>
2021-01-02 16:43:37 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: true,
-  firebaseConfig : {
-    apiKey: 'AIzaSyCbRcLzjU2A4lJtOxNVBve-p7AJiYB32vc',
-    authDomain: 'instan-transfer-management.firebaseapp.com',
-    databaseURL: 'https://instan-transfer-management.firebaseio.com',
-    projectId: 'instan-transfer-management',
-    storageBucket: 'instan-transfer-management.appspot.com',
-    messagingSenderId: '430459546960',
-    appId: '1:430459546960:web:7e58d144cd3b89532f1055',
-    measurementId: 'G-457HD49VE7'
-  }
 };
 
 /*
